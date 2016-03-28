bbff378eb4217ab21435c964a9eaa4ed7ef66903
Jean Farias Roldão <jeanroldao@gmail.com>
2016-03-28 00:18:35 -0300

    Layout components
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var elem = [4, 8, 5];
-
-class Layout extends React.Component {
-  
-  render() {
-    return (
-      <h1>Works, it seems!!</h1>
-    );
-  }
-}
+import Layout from './components/Layout';
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout />, app);
+ReactDOM.render(<Layout/>, app);
