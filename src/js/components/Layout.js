bbc44f217935d5918573a59cdd512ab86d7138ef
Jean Farias Roldão <jeanroldao@gmail.com>
2016-03-29 22:13:38 -0300

    Code format
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -8,12 +8,10 @@ export default class Layout extends React.Component {
   constructor() {
     super();
     this.state = { name: "Jean" };
-
   }
   
   render() {
 
-    
     return (
       <div>
         <Header title={this.state.name} />
